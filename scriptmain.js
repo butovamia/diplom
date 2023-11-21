4642d7bd891b5ddbb74d99d5f1604481b7e6f5e0
Ivan Nimych <in7264@gmail.com>
2023-11-21 21:44:44 +0200

    1.25v

--- scriptmain.js
+++ scriptmain.js
@@ -26,3 +26,72 @@ function openTab(evt, tabName) {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
+
+
+function createOffer(event) {
+    event.preventDefault();
+
+    // Получаем значения полей из формы
+    const companyName = document.getElementById("companyName").value;
+    const offerName = document.getElementById("offerName").value;
+    const salary = document.getElementById("salary").value;
+    const language = document.getElementById("language").value;
+    const technologies = document.getElementById("technologies").value;
+
+    // Создаем объект с данными оффера
+    const offerData = {
+        companyName: companyName,
+        offerName: offerName,
+        salary: salary,
+        language: language,
+        technologies: technologies,
+    };
+
+    // Отправляем данные на сервер (замените на свои данные и укажите правильный путь к offers.json)
+    fetch('https://api.github.com/repos/butovamia/diplom/contents/offers.json')
+        .then(response => response.json())
+        .then(existingData => {
+            const existingOffersString = atob(existingData.content);
+            let existingOffers;
+
+            try {
+                existingOffers = JSON.parse(existingOffersString);
+
+                if (!Array.isArray(existingOffers)) {
+                    existingOffers = [existingOffers];
+                }
+            } catch (error) {
+                existingOffers = [];
+            }
+
+            // Добавляем новый оффер
+            existingOffers.push(offerData);
+
+            // Обновляем содержимое файла с учетом новых данных
+            const updatedContent = btoa(JSON.stringify(existingOffers));
+            const githubToken = 'ghp_B25NgQ3Z8M7k9tU5dlD5Kc';
+
+            fetch('https://api.github.com/repos/butovamia/diplom/contents/offers.json', {
+                method: 'PUT',
+                headers: {
+                    'Authorization': `Bearer ${githubToken + 'cSi0obAX0fKZVB'}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    message: 'Добавление нового оффера',
+                    content: updatedContent,
+                    sha: existingData.sha,
+                }),
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Данные успешно отправлены на GitHub:', data);
+            })
+            .catch(error => {
+                console.error('Ошибка при обновлении данных на GitHub:', error);
+            });
+        })
+        .catch(error => {
+            console.error('Ошибка при получении предыдущей версии файла:', error);
+        });
+}
