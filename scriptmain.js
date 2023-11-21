dff844e6c1122fae156a9ecafeb23de4a567182c
Ivan Nimych <in7264@gmail.com>
2023-11-21 22:33:59 +0200

    1.30v

--- scriptmain.js
+++ scriptmain.js
@@ -11,6 +11,12 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById("username").textContent = `Логин: ${userData.username}`;
         document.getElementById("userRole").textContent = `Роль: ${decodeURIComponent(userData.role)}`;
     }
+
+        // По умолчанию открываем первую вкладку
+        document.getElementById("Tab1").style.display = "block";
+    
+        // Отображаем оферы при загрузке страницы
+        showOffers();
 });
 
 function openTab(evt, tabName) {
@@ -27,6 +33,28 @@ function openTab(evt, tabName) {
     evt.currentTarget.className += " active";
 }
 
+function showNotification(message) {
+    const notification = document.getElementById('notification');
+    const notificationMessage = document.getElementById('notification-message');
+  
+    // Устанавливаем текст уведомления
+    notificationMessage.textContent = message;
+  
+    // Показываем уведомление
+    notification.classList.remove('hide');
+  
+    // Через 5 секунд скрываем уведомление
+    setTimeout(() => {
+      hideNotification();
+    }, 5000);
+  }
+  
+  // Функция для скрытия уведомления
+  function hideNotification() {
+    const notification = document.getElementById('notification');
+    notification.classList.add('hide');
+  }
+  
 
 function createOffer(event) {
     event.preventDefault();
@@ -46,6 +74,7 @@ function createOffer(event) {
         language: language,
         technologies: technologies,
     };
+    showNotification('Офер успешно создан!');
 
     // Отправляем данные на сервер (замените на свои данные и укажите правильный путь к offers.json)
     fetch('https://api.github.com/repos/butovamia/diplom/contents/offers.json')
@@ -93,5 +122,55 @@ function createOffer(event) {
         })
         .catch(error => {
             console.error('Ошибка при получении предыдущей версии файла:', error);
+        });
+        
+}
+
+function showOffers() {
+    // Получаем данные о оферах из файла offers.json
+    fetch('https://api.github.com/repos/butovamia/diplom/contents/offers.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Ошибка при получении данных с сервера');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const offersString = atob(data.content);
+            let offers;
+
+            try {
+                offers = JSON.parse(offersString);
+
+                if (!Array.isArray(offers)) {
+                    offers = [offers];
+                }
+            } catch (error) {
+                offers = [];
+            }
+
+            // Очищаем список оферов
+            const offersList = document.getElementById('offers');
+            offersList.innerHTML = '';
+
+            // Выводим каждый офер
+            offers.forEach(offer => {
+                const offerItem = document.createElement('div');
+                offerItem.classList.add('offer-item');
+
+                const content = `
+                    <p>Компания: ${offer.companyName}</p>
+                    <p>Офер: ${offer.offerName}</p>
+                    <p>Зарплата: ${offer.salary}</p>
+                    <p>Язык: ${offer.language}</p>
+                    <p>Технологии: ${offer.technologies}</p>
+                `;
+
+                offerItem.innerHTML = content;
+                offersList.appendChild(offerItem);
+            });
+        })
+        .catch(error => {
+            console.error('Ошибка при получении данных о оферах:', error);
         });
 }
